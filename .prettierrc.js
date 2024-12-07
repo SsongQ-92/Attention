c01c4dbd0c9fb2b06340f2eee40c98377de1f79b
Ssong-Q <skdmlrh12@gmail.com>
2024-12-07 14:16:10 +0900

    ➕ [Chore] react-hooks에 대한 eslint 플러그인 추가

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,6 @@
 /**
  * @type {import('prettier').Options}
  */
-
 export default {
   trailingComma: 'es5',
   semi: true,
--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,23 @@
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import eslintConfigPrettier from "eslint-config-prettier";
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    plugins: { 'react-hooks': pluginReactHooks },
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+  },
   eslintConfigPrettier,
 ];
