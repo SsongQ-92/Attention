f995ec39d05fe2f783454d5ad97a11c86b0df539
Ssong-Q <skdmlrh12@gmail.com>
2024-12-09 14:00:28 +0900

    ♻️ [Refactor] content-script의 index.tsx 유효를 위해서 관련 코드 index.tsx로 이동

--- src/content-script/index.tsx
+++ src/content-script/index.tsx
@@ -1,3 +1,48 @@
+import { StrictMode } from 'react';
+
+import { createRoot } from 'react-dom/client';
+
+import DashboardContainer from '../components/Dashboard/DashboardContainer';
+import ServiceHighlightLayer from '../components/ServiceHighlight/ServiceHighlightLayer';
+
+class WebHighlightLayer extends HTMLElement {
+  constructor() {
+    super();
+
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+
+    const reactRoot = document.createElement('div');
+    shadowRoot.appendChild(reactRoot);
+
+    createRoot(reactRoot).render(
+      <StrictMode>
+        <ServiceHighlightLayer />
+      </StrictMode>
+    );
+  }
+}
+
+customElements.define('web-highlight-layer', WebHighlightLayer);
+
+class WebDashboard extends HTMLElement {
+  constructor() {
+    super();
+
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+
+    const reactRoot = document.createElement('div');
+    shadowRoot.appendChild(reactRoot);
+
+    createRoot(reactRoot).render(
+      <StrictMode>
+        <DashboardContainer />
+      </StrictMode>
+    );
+  }
+}
+
+customElements.define('web-dashboard', WebDashboard);
+
 const webHighlightLayer = document.createElement('web-highlight-layer');
 document.body.appendChild(webHighlightLayer);
 
