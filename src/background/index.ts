2be1d23e0bc2df63f1a18c7fafc582a26eb7d933
Ssong-Q <skdmlrh12@gmail.com>
2024-12-10 18:02:32 +0900

    ✨ [Feat] 단축키에 대한 대시보드 UI 토글 로직 구현

--- src/background/index.ts
+++ src/background/index.ts
@@ -36,12 +36,31 @@ Browser.tabs.onActivated.addListener(async (activeInfo) => {
   }
 });
 
-Browser.action.onClicked.addListener(async (tab: Browser.Tabs.Tab) => {
+Browser.action.onClicked.addListener((tab: Browser.Tabs.Tab) => {
   for (const url of ALLOW_URL) {
     if (tab?.url?.includes(url) && tab.id) {
       Browser.tabs.sendMessage(tab.id, { action: msgAction.ICON_CLICKED });
 
       break;
+    }
+  }
+});
+
+Browser.commands.onCommand.addListener(async (command) => {
+  if (command === '_execute_action') {
+    try {
+      const tabs = await Browser.tabs.query({ active: true, currentWindow: true });
+      const currentTab = tabs[0];
+
+      for (const url of ALLOW_URL) {
+        if (currentTab?.url?.includes(url) && currentTab.id) {
+          Browser.tabs.sendMessage(currentTab.id, { action: msgAction.ICON_CLICKED });
+
+          break;
+        }
+      }
+    } catch (error) {
+      console.error('tabs.query 실패:', error);
     }
   }
 });
