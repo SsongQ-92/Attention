bc417bef1e3c13a7f8aef83f4cf812fd14daef0b
Kyukyoung Song (James Song) <skdmlrh12@gmail.com>
2024-12-13 16:20:15 +0900

    💄 [Design] 키보드 안내 문구 position 수정

--- src/components/ServiceHighlight/ServiceHighlightLayer.tsx
+++ src/components/ServiceHighlight/ServiceHighlightLayer.tsx
@@ -13,18 +13,18 @@ function ServiceHighlightLayer() {
     highlightLayerInfo.width !== 0;
 
   return (
-    <main className={`${isKeyboardMode ? 'fixed inset-0' : 'relative'} w-screen h-screen`}>
+    <>
       {isHighlightBarOpen && isKeyboardMode && (
-        <div className='fixed top-20 w-full flex-center'>
-          <div className='flex-center text-center bg-white text-red-600 p-10 border-customBlack border-2 rounded-md text-12 opacity-30 hover:opacity-100'>
+        <div className='fixed left-0 z-50 top-20 w-full flex-center'>
+          <div className='flex-center text-center bg-white text-red-600 p-10 border-customBlack border-2 rounded-md text-12 opacity-40 hover:opacity-100'>
             현재 키보드 모드입니다. 키보드 방향키(↑, ↓)로 움직여 주세요. <br />
             모드를 종료하시려면 하이라이트 바 / 키보드 아이콘 / ESC 키를 눌러주세요.
           </div>
         </div>
       )}
       {isHighlightBarOpen && !isKeyboardMode && (
-        <div className='fixed top-20 w-full flex-center'>
-          <div className='flex-center text-center bg-white text-red-600 p-10 border-customBlack border-2 rounded-md text-12 opacity-30 hover:opacity-100'>
+        <div className='fixed left-0 z-50 top-20 w-full flex-center'>
+          <div className='flex-center text-center bg-white text-red-600 p-10 border-customBlack border-2 rounded-md text-12 opacity-40 hover:opacity-100'>
             키보드 모드 진입을 위해서는 <br />
             하이라이트 바나 키보드 이모지를 눌러주세요.
           </div>
@@ -41,7 +41,7 @@ function ServiceHighlightLayer() {
           className='fixed z-40 bg-yellow-400/20'
         />
       )}
-    </main>
+    </>
   );
 }
 
