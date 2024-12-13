252b276a41a6fdcafa42a8138daf933013504781
Ssong-Q <skdmlrh12@gmail.com>
2024-12-13 12:18:31 +0900

    ✨ [Feat] memo 작성 기본 동작 구현 완료

--- src/components/Dashboard/DashboardContainer.tsx
+++ src/components/Dashboard/DashboardContainer.tsx
@@ -1,6 +1,7 @@
 import { SERVICE_TITLE } from '../../config/consts';
 import useToggleDashboard from '../../hooks/useToggleDashboard';
 import useBoundStore from '../../store/useBoundStore';
+import MemoEditor from '../Memo/MemoEditor';
 import FoldedDashboard from './FoldedDashboard';
 
 function DashboardContainer() {
@@ -12,10 +13,11 @@ function DashboardContainer() {
   return (
     <main className='h-screen fixed top-0 left-0 z-50'>
       {isDashboardOpen && (
-        <div className='flex flex-col justify-start gap-50 p-10 py-30 w-270 h-full bg-white'>
+        <div className='flex flex-col justify-start gap-20 p-10 py-30 w-270 h-full bg-white'>
           <h1 className='font-pretendard color-customBlack text-25 bg-yellow-100 px-10 font-bold'>
             {SERVICE_TITLE}
           </h1>
+          <MemoEditor />
         </div>
       )}
       <FoldedDashboard />
--- src/components/Memo/MemoEditor.tsx
+++ src/components/Memo/MemoEditor.tsx
@@ -1,7 +1,6 @@
 import { ChangeEvent, useState } from 'react';
 
 import Markdown from 'react-markdown';
-import rehypePrettyCode from 'rehype-pretty-code';
 import remarkGfm from 'remark-gfm';
 
 import 'github-markdown-css';
@@ -14,22 +13,17 @@ function MemoEditor() {
   };
 
   return (
-    <div className='w-full h-full flex flex-col gap-30'>
+    <div className='w-full h-full flex flex-col gap-10'>
       <textarea
         name='markdownEditor'
         id='markdownEditor'
-        className='w-full flex-grow border-1 rounded-sm p-5 border-borderColor'
+        className='w-full flex-grow border-1 rounded-sm p-5 border-borderColor resize-none overflow-y-auto'
         value={markdown}
         onChange={handleTextareaChange}
       />
 
       <div className='prose prose-sm max-w-none w-full flex-grow border-1 rounded-sm p-5 border-borderColor'>
-        <Markdown
-          remarkPlugins={[remarkGfm]}
-          rehypePlugins={[[rehypePrettyCode, { theme: 'github-dark' }]]}
-        >
-          {markdown}
-        </Markdown>
+        <Markdown remarkPlugins={[remarkGfm]}>{markdown}</Markdown>
       </div>
     </div>
   );
