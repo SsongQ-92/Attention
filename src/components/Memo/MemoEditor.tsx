04d146436906e35934f9fa7461da2554817f7d6e
Ssong-Q <skdmlrh12@gmail.com>
2024-12-14 15:49:26 +0900

    💄 [Design] 메모 에디터 및 마크다운 프리뷰 디자인 수정

--- src/components/Memo/MemoEditor.tsx
+++ src/components/Memo/MemoEditor.tsx
@@ -17,13 +17,12 @@ function MemoEditor() {
       <textarea
         name='markdownEditor'
         id='markdownEditor'
-        className='w-full flex-grow border-1 rounded-sm p-5 border-borderColor resize-none overflow-y-auto'
+        className='w-full flex-1 border-1 rounded-sm p-5 border-borderColor resize-none overflow-y-auto'
         placeholder='[마크다운 지원] 텍스트를 입력해주세요.'
         value={markdown}
         onChange={handleTextareaChange}
       />
-
-      <div className='prose prose-sm max-w-none w-full flex-grow border-1 rounded-sm p-5 border-borderColor'>
+      <div className='prose prose-sm max-w-none w-full flex-1 border-1 rounded-sm p-5 border-borderColor overflow-y-auto'>
         <Markdown remarkPlugins={[remarkGfm]}>{markdown}</Markdown>
       </div>
     </div>
