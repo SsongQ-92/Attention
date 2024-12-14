386a07d820c5b80c5c70f1b52675e5a43ab993b9
Ssong-Q <skdmlrh12@gmail.com>
2024-12-14 17:39:34 +0900

    ✨ [Feat] 메모 작성에 대한 리스트 및 저장 버튼 UI 구현

--- src/components/Memo/MemoEditor.tsx
+++ src/components/Memo/MemoEditor.tsx
@@ -5,26 +5,52 @@ import remarkGfm from 'remark-gfm';
 
 import 'github-markdown-css';
 
-function MemoEditor() {
-  const [markdown, setMarkdown] = useState<string>('');
+import ListIcon from '../Icon/ListIcon';
 
-  const handleTextareaChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setMarkdown(e.target.value);
+function MemoEditor() {
+  const [markdownTitle, setMarkdownTitle] = useState<string>('');
+  const [markdownContent, setMarkdownContent] = useState<string>('');
+
+  const handleSaveClick = () => {};
+
+  const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setMarkdownTitle(e.target.value);
+  };
+
+  const handleContentChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setMarkdownContent(e.target.value);
   };
 
   return (
     <div className='w-full h-full flex flex-col gap-10'>
+      <div className='flex justify-between items-center'>
+        <div className='flex-center rounded-[5px] size-26 hover:bg-backgroundColor-hover'>
+          <ListIcon className='size-22 hover:cursor-pointer' />
+        </div>
+        <button
+          className='py-3 px-5 rounded-sm border-1 border-borderColor text-customBlack text-14 hover:bg-backgroundColor-hover '
+          onClick={handleSaveClick}
+        >
+          저장
+        </button>
+      </div>
+      <input
+        className='w-full h-30 flex-shrink-0 border-1 rounded-sm p-5 border-borderColor placeholder:text-15 text-15'
+        placeholder='제목을 입력해주세요.'
+        value={markdownTitle}
+        onChange={handleTitleChange}
+      />
       <textarea
         name='markdownEditor'
         id='markdownEditor'
-        className='w-full flex-1 border-1 rounded-sm p-5 border-borderColor resize-none overflow-y-auto'
+        className='w-full flex-1 border-1 rounded-sm p-5 border-borderColor resize-none overflow-y-auto placeholder:text-15 text-15'
         placeholder='[마크다운 지원] 텍스트를 입력해주세요.'
-        value={markdown}
-        onChange={handleTextareaChange}
+        value={markdownContent}
+        onChange={handleContentChange}
       />
-      <div className='prose prose-sm max-w-none w-full flex-1 border-1 rounded-sm p-5 border-borderColor overflow-y-auto'>
-        <Markdown remarkPlugins={[remarkGfm]}>{markdown}</Markdown>
-      </div>
+      <article className='prose-sm max-w-none w-full flex-1 border-1 rounded-sm p-5 border-borderColor overflow-y-auto'>
+        <Markdown remarkPlugins={[remarkGfm]}>{markdownContent}</Markdown>
+      </article>
     </div>
   );
 }
