681045216189ec768a681ddd79f50bcd1f3797a9
Ssong-Q <skdmlrh12@gmail.com>
2024-12-16 16:30:28 +0900

    💄 [Design] fixed 요소에 pointer-events 관련 적용

--- src/components/ServiceHighlight/ServiceHighlightLayer.tsx
+++ src/components/ServiceHighlight/ServiceHighlightLayer.tsx
@@ -15,16 +15,16 @@ function ServiceHighlightLayer() {
   return (
     <>
       {isHighlightBarOpen && isKeyboardMode && (
-        <div className='fixed left-0 z-50 top-20 w-full flex-center'>
-          <div className='flex-center text-center bg-white text-red-600 p-10 border-customBlack border-2 rounded-md text-12 opacity-40 hover:opacity-100'>
+        <div className='fixed left-0 z-50 top-20 w-full flex-center pointer-events-none'>
+          <div className='flex-center text-center bg-white text-red-600 p-10 border-customBlack border-2 rounded-md text-12 opacity-40 pointer-events-auto hover:opacity-100'>
             현재 키보드 모드입니다. 키보드 방향키(↑, ↓)로 움직여 주세요. <br />
             모드를 종료하시려면 하이라이트 바 / 키보드 아이콘 / ESC 키를 눌러주세요.
           </div>
         </div>
       )}
       {isHighlightBarOpen && !isKeyboardMode && (
-        <div className='fixed left-0 z-50 top-20 w-full flex-center'>
-          <div className='flex-center text-center bg-white text-red-600 p-10 border-customBlack border-2 rounded-md text-12 opacity-40 hover:opacity-100'>
+        <div className='fixed left-0 z-50 top-20 w-full flex-center pointer-events-none'>
+          <div className='flex-center text-center bg-white text-red-600 p-10 border-customBlack border-2 rounded-md text-12 opacity-40 pointer-events-auto hover:opacity-100'>
             키보드 모드 진입을 위해서는 <br />
             하이라이트 바나 키보드 이모지를 눌러주세요.
           </div>
