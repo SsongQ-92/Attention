248c2432c428866e55ed7f3ac570eefe73b62699
Ssong-Q <skdmlrh12@gmail.com>
2025-01-02 23:13:16 +0900

    💄 [Design] 키보드 모드 진입 알람의 투명도 제거

--- src/components/ServiceHighlight/ServiceHighlightLayer.tsx
+++ src/components/ServiceHighlight/ServiceHighlightLayer.tsx
@@ -25,7 +25,7 @@ function ServiceHighlightLayer() {
       )}
       {isHighlightBarOpen && !isKeyboardMode && isKeyboardIconHover && (
         <div className='fixed left-0 z-50 top-20 w-full flex-center pointer-events-none'>
-          <div className='flex-center text-center bg-white text-red-600 p-10 border-customBlack border-2 rounded-md text-12 opacity-40 pointer-events-auto hover:opacity-100'>
+          <div className='flex-center text-center bg-white text-red-600 p-10 border-customBlack border-2 rounded-md text-12 pointer-events-auto'>
             키보드 모드 진입을 위해서는 <br />
             하이라이트 바나 키보드 이모지를 눌러주세요.
           </div>
